463158213cb2101928cd7b903d3028d247829e62
Sang Oh <baul3oh@gmail.com>
2022-11-12 00:21:53 -0600

    signupForm and loginForm complete"

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,7 +7,7 @@ import {
   Button,
 } from "react-bootstrap";
 
-import { getMe, deleteBook } from "../utils/API";
+// import { getMe, deleteBook } from "../utils/API";
 import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
